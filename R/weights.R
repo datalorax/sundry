3ee85c43be9459a7316abdf3e986886bc781d365
Daniel Anderson <daniela@uoregon.edu>
2018-11-08 16:24:06 -0800

    Updated output to be a df for aic_weights

--- R/weights.R
+++ R/weights.R
@@ -1,13 +1,22 @@
 #' Calculate Akaike Weights for a set of models.
 #'
-#' @param ... The candidate models to compare
-#' @return Vector of Akaike weights
+#' @param ... The models to compare
+#' @return A data frame containing the delta AIC values and corresponding
+#'   weights for each model
 #' @export
 
 aic_weights <- function(...) {
   l <- list(...)
   aics <- vapply(l, AIC, numeric(1))
+  formulas <- vapply(l, function(x) {
+    as.character(formula(x))[3]
+    },
+    character(1))
   delta <- aics - min(aics)
 
-  round(exp(-delta/2) / sum(exp(-delta/2)), 2)
+  weights <- round(exp(-delta/2) / sum(exp(-delta/2)), 2)
+  out <- data.frame(predictors  = formulas,
+             delta  = delta,
+             weight = weights)
+  out[order(out$weight, decreasing = TRUE), ]
 }
